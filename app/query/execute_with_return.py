08816ac4c162d705158b5c78d3a28a417cc4eeb4
MatthewLeigh <matthewcross97@gmail.com>
2025-07-29 15:13:32 +1000

    feat: accept db connection for multiple statement transactions

--- app/query/execute_with_return.py
+++ app/query/execute_with_return.py
@@ -1,9 +1,12 @@
 from app.db.connection import get_db_connection
 
-def execute(query: str, values: tuple = ()) -> tuple:
-    with get_db_connection() as connection:
-        with connection.cursor() as cursor:
-            cursor.execute(query, values)
-            result = cursor.fetchone()
-            connection.commit()
-            return result
+def execute(query: str, *values, connection=None, cursor=None):
+    if connection and cursor:
+        cursor.execute(query, values)
+        return cursor.fetchone()
+    else:
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query, values)
+                connection.commit()
+                return cursor.fetchone()
