0b8a1b18215db63e4dc65651caa3944a9721bd85
MatthewLeigh <matthewcross97@gmail.com>
2025-07-29 17:39:14 +1000

    feat: put venue endpoint

--- app/api/venue.py
+++ app/api/venue.py
@@ -8,6 +8,8 @@ from app.model.venue_insert import VenueInsert
 from app.rest.venue.get import get_complete
 from app.rest.venue.get_all_id import get_all_id
 from app.rest.venue.post import post
+from app.rest.venue.put import put
+from app.rest.venue.delete import delete
 
 
 venue = APIRouter()
@@ -39,3 +41,13 @@ def get_all_id_(
 @venue.post("/", response_model=UUID, status_code=status.HTTP_201_CREATED)
 def post_(venue: VenueInsert = Body(...)):
     return post(venue)
+
+# PUT
+@venue.put("/{venue_id}", status_code=status.HTTP_204_NO_CONTENT)
+def put_(venue_id: UUID, venue: VenueInsert = Body(...)):
+    return put(venue_id, venue)
+
+# DELETE
+@venue.delete("/{venue_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_(venue_id: UUID):
+    return delete(venue_id)
---
+++ app/rest/venue/put.py
@@ -0,0 +1,71 @@
+from app.query.execute import execute
+from app.db.connection import get_db_connection
+from psycopg2 import DatabaseError
+
+from app.rest.venue.secondary_tables.post_venue_type import post_venue_type
+from app.rest.venue.secondary_tables.post_venue_tag import post_venue_tag
+from app.rest.venue.secondary_tables.post_venue_social import post_venue_social
+from app.rest.venue.secondary_tables.post_venue_image import post_venue_image
+from app.rest.venue.secondary_tables.post_venue_stages import post_venue_stages
+from app.rest.venue.secondary_tables.post_venue_opening_hours import post_venue_opening_hours
+
+from app.model.venue_insert import VenueInsert
+from uuid import UUID
+
+
+def put(venue_id: UUID, venue: VenueInsert) -> None:
+    with get_db_connection() as connection:
+        with connection.cursor() as cursor:
+            try:
+                update_venue(venue_id, venue, connection, cursor)
+                delete_related_venue_data(venue_id, connection, cursor)
+                post_venue_type(venue.Types, venue_id, connection, cursor)
+                post_venue_tag(venue.Tags, venue_id, connection, cursor)
+                post_venue_social(venue.Socials, venue_id, connection, cursor)
+                post_venue_image(venue.ImageUrls, venue_id, connection, cursor)
+                post_venue_stages(venue.VenueStages, venue_id, connection, cursor)
+                post_venue_opening_hours(venue.OpeningHours, venue_id, connection, cursor)
+                connection.commit()
+            except DatabaseError:
+                connection.rollback()
+                raise
+
+
+def update_venue(venue_id: UUID, venue: VenueInsert, connection, cursor) -> None:
+    execute(
+    """
+        UPDATE Venue SET
+            Title = %s,
+            StreetAddress = %s,
+            City = %s,
+            StateCode = %s,
+            PostCode = %s,
+            Description = %s,
+            WebsiteURL = %s,
+            PhoneNumber = %s,
+            GoogleMapsEmbedURL = %s
+        WHERE VenueID = %s
+    """,
+    (
+        venue.Title,
+        venue.StreetAddress,
+        venue.City,
+        venue.StateCode,
+        venue.PostCode,
+        venue.Description,
+        str(venue.WebsiteUrl) if venue.WebsiteUrl else None,
+        venue.PhoneNumber,
+        str(venue.GoogleMapsEmbedUrl),
+        str(venue_id)
+    ),
+        connection=connection,
+        cursor=cursor
+    )
+
+def delete_related_venue_data(venue_id: UUID, connection, cursor):
+    execute("DELETE FROM VenueType WHERE VenueID = %s", (str(venue_id),), connection=connection, cursor=cursor)
+    execute("DELETE FROM VenueTag WHERE VenueID = %s", (str(venue_id),), connection=connection, cursor=cursor)
+    execute("DELETE FROM VenueSocial WHERE VenueID = %s", (str(venue_id),), connection=connection, cursor=cursor)
+    execute("DELETE FROM VenueImage WHERE VenueID = %s", (str(venue_id),), connection=connection, cursor=cursor)
+    execute("DELETE FROM VenueOpeningHours WHERE VenueID = %s", (str(venue_id),), connection=connection, cursor=cursor)
+    execute("DELETE FROM VenueStage WHERE VenueID = %s", (str(venue_id),), connection=connection, cursor=cursor)
