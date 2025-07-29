09d0505a5e79f8f601378d44b6eae507a5fc9b10
MatthewLeigh <matthewcross97@gmail.com>
2025-07-29 13:57:31 +1000

    feat: remove artist brief

--- app/api/artist.py
+++ app/api/artist.py
@@ -2,26 +2,14 @@ from fastapi import APIRouter, Query
 
 from typing import Optional
 from uuid import UUID
-from app.model.artist_brief import ArtistBrief
 from app.model.artist import Artist
 
-from app.rest.artist.get_brief import get_brief
-from app.rest.artist.get_all_brief import get_all_brief
 from app.rest.artist.get import get_complete
 from app.rest.artist.get_all_id import get_all_id
 
 
 artist = APIRouter()
 
-# GET Single brief
-@artist.get("/brief/{artist_id}", response_model=ArtistBrief)
-def get_brief_(artist_id: UUID):
-    return get_brief(artist_id)
-
-# GET All briefs
-@artist.get("/brief", response_model=list[ArtistBrief])
-def get_all_brief_():
-    return get_all_brief()
 
 # GET Single Complete
 @artist.get("/{artist_id}", response_model=Artist)
