82954663c97f3cb3a23c002e5ce65aa13b9408ef
MatthewLeigh <matthewcross97@gmail.com>
2025-09-09 13:05:19 +1000

    feat: extend get_all_id_and_title to include StageID and StageTitle

--- app/rest/venue/get_all_id_and_title.py
+++ app/rest/venue/get_all_id_and_title.py
@@ -3,13 +3,18 @@ from app.query.fetch_list import execute
 
 def get_all_id_and_title() -> List[dict]:
     response = execute(query())
-    return [{"VenueID": row[0], "Title": row[1]} for row in response]
+    return [{"VenueID": row[0], "Title": row[1], "StageID": row[2], "StageTitle": row[3]} for row in response]
 
 def query():
     return """
         SELECT
-            VenueID,
-            Title 
+            Venue.VenueID,
+            Venue.Title,
+            VenueStage.StageID,
+            VenueStage.Title
         FROM Venue
-        ORDER BY Title
+        JOIN VenueStage ON VenueStage.VenueID = Venue.VenueID
+        ORDER BY 
+            Venue.Title, 
+            VenueStage.Title
     """
