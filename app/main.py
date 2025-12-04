4c99f42d2adf508c204eb58084d9ef31a821fc82
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-12-04 13:27:09 +1100

    refactor: remove search endpoint
    moved to a dedicated microservice called `service-search`

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ from fastapi.exceptions import HTTPException
 from app.api.artist import artist
 from app.api.event import event
 from app.api.venue import venue
-from app.api.search import search
 
 from app.util.test_db_connection import test_db_connection
 
@@ -18,7 +17,6 @@ app = FastAPI()
 app.include_router(artist, prefix="/artist", tags=["artist"])
 app.include_router(event, prefix="/event", tags=["event"])
 app.include_router(venue, prefix="/venue", tags=["venue"])
-app.include_router(search, prefix="/search", tags=["search"])
 
 # Handlers
 app.add_exception_handler(Exception, handle_exception)
